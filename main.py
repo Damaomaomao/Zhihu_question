9097e3fa538e327f8b07fd9f74398c6efdf50dfe
Damaomaomao <40875511+Damaomaomao@users.noreply.github.com>
2018-08-05 14:27:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,4 @@ from scrapy.cmdline import execute
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
-    #execute(["scrapy", "crawl", "zhihu"])
     execute(["scrapy", "crawl", "zhihucrawl"])
